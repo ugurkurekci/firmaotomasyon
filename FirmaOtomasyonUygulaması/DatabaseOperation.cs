079e3d66b0de2862294de1616b0e61a7bae1e869
ugurkurekci <ugurkurekci98@gmail.com>
2021-02-07 19:56:36 +0300

    Updated

---
+++ FirmaOtomasyonUygulaması/DatabaseOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace FirmaOtomasyonUygulaması
+{
+    public class DatabaseOperation : MusteriListele
+    {
+        void databaseoperation()
+        {
+            using (firmaEntities Firmamüsteri = new firmaEntities())
+            {
+                DataGridView.DataSource = Firmamüsteri.müsteriKayit.ToList<müsteriKayit>();
+            }
+        }
+    }
+}
